7e9d91ca607ea8b72739ddccd74042f9a09a1666
Djibril-Mugisho <mugishodjibril2004@gmail>
2023-08-07 18:36:09 +0300

    added store functions

--- src/store/features/todo.ts
+++ src/store/features/todo.ts
@@ -6,6 +6,8 @@ interface Todo {
     content: string,
 }
 
+
+
 interface Store {
     todos: Todo[],
     length: number,
@@ -13,15 +15,29 @@ interface Store {
     markCompleted: (id: string) => void;
     deleteTodo: (id: string) => void,
     clearAllCompleted: () => void
+
 }
 
-const store = create<Store>((set, get) => ({
+
+const useTodoStore = create<Store>((set, get) => ({
     todos: [],
     length: 0,
-    addTodo: (newTodo) => set({}),
-    markCompleted: (todoId) => set({}),
-    deleteTodo: (todoId) => set({}),
-    clearAllCompleted: () => set({})
+    addTodo: (newTodo) => set({ todos: [...get().todos, newTodo], length: get().todos.length }),
+    markCompleted: (todoId) => {
+        const getTodos = get().todos  // get the list of all todos avalaible in the store
+
+        const todoIndex = getTodos.findIndex((todo) => todo.id === todoId);  //find todo index
+
+        getTodos[todoIndex].completed = !getTodos[todoIndex];  //update the complete property by its oposite.
+
+        set({ todos: [...getTodos], length: get().todos.length });  //update the list all todos in the store with the new changes.
+
+    },
+
+    deleteTodo: (todoId) => set({ todos: get().todos.filter((todo) => todo.id !== todoId) }),  //delete the targeted todo.
+
+    clearAllCompleted: () => set({ todos: [] })
+
 }))
 
-export default store;
+export default useTodoStore;
